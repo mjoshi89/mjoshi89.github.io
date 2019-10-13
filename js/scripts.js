04e88a3bf6a36879010bbc3017525105fbbc41d0
mjoshi89 <manish.a.joshi@accenture.com>
2019-10-13 14:01:06 +0100

    adding gsheet support

--- js/scripts.js
+++ js/scripts.js
@@ -184,23 +184,23 @@ $(document).ready(function () {
         },
         data: {
             // Event title
-            title: "Ram and Antara's Wedding",
+            title: "Manish & Krishna's Wedding",
 
             // Event start date
-            start: new Date('Nov 27, 2017 10:00'),
+            start: new Date('Dec 8, 2019 09:00'),
 
             // Event duration (IN MINUTES)
             // duration: 120,
 
             // You can also choose to set an end time
             // If an end time is set, this will take precedence over duration
-            end: new Date('Nov 29, 2017 00:00'),
+            end: new Date('Dec 12, 2019 00:00'),
 
             // Event Address
-            address: 'ITC Fortune Park Hotel, Kolkata',
+            address: 'Kochi, Kerala and Haldwani Uttarakhand, India',
 
             // Event Description
-            description: "We can't wait to see you on our big day. For any queries or issues contact Mr. Amit Roy at +91 9435021804 or +91 7086018971."
+            description: "We can't wait to see you on our big day. For any queries or issues contact Manish Joshi at +91 7718947824 or +44 7852386732."
         }
     });
 
